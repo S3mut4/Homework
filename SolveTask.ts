2c2cb847932a0a11150d3dd5c881265c78a11b13
C0r3dump <daniel.castells@gmail.com>
2026-09-10 22:40:18 +0300

    Update homework solution

--- SolveTask.ts
+++ SolveTask.ts
@@ -1,3 +1,20 @@
+//////////////////////////////////////////////////////////////
+//#Create new proyect
+//npm init -y          #Default Node package.json options
+//npx tsc --init.      #Default TypeScript.json options
+//
+//##################
+//#And then modify package.json from "type": "commonjs" to "type": "module". # #enabling the native export and import syntax.
+//#
+//#With this configuration, export works correctly, and I don't need to install #TypeScript separately in every project folder.
+//#
+//#**Note: In TypeScript.json "types": [], With this TypeScript will not provide #autocomplete for Node.js global variables such as process.env or Buffer (Forces #explicit imports and improves performance).
+//##################
+//
+//#Run typecheck command before execution
+//npx tsc --noEmit
+//////////////////////////////////////////////////////////////
+
 // --- Task 1: Calculate the area of a rectangle ---
 // Definition
 let length: number = 4;
