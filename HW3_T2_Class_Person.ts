418c7b83f45bab6a641b1f6a7bd35b35ee949f70
C0r3dump <daniel.castells@gmail.com>
2026-09-11 19:51:42 +0300

    Modification

--- HW3_T2_Class_Person.ts
+++ HW3_T2_Class_Person.ts
@@ -15,3 +15,9 @@ const person2 = new Person("Mundo", "Hola");
 
 console.log("Person 1:", person1);
 console.log("Person 2:", person2);
+
+// Modify an existing instance of one class
+const person3 = new Person("Hola", "Mundo");
+console.log(person3.firstName);
+person3.firstName = "aloH";
+console.log(person3.firstName);
