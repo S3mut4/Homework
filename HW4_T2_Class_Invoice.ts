bc87bb0c2e4d24f7588b133c8dc4ceea09ab0548
C0r3dump <daniel.castells@gmail.com>
2026-09-17 11:15:30 +0300

    HW4_Yutani_corp

--- HW4_T2_Class_Invoice.ts
+++ HW4_T2_Class_Invoice.ts
@@ -5,7 +5,7 @@ class Invoice {
     amount: number
     status: string
 
-    constructor( invoiceNumber: string, clientName: string, amount: number, status: string) {
+    constructor(invoiceNumber: string, clientName: string, amount: number, status: string) {
         this.invoiceNumber = invoiceNumber
         this.clientName = clientName
         this.amount = amount
@@ -26,17 +26,18 @@ class Invoice {
 
 // Verification
 
-// 1. Invoice instance pending
-const myInvoice = new Invoice("INV-101", "Acme Corp", 500, "Pending")
-console.log("Status inicial:", myInvoice.status)
+// 1. Invoice instance pending for Yutani Corp with a big number
+const myInvoice = new Invoice("WY-2179", "Yutani Corp", 42_000_000_000, "Pending")
+console.log("Initial status:", myInvoice.status)
 
 // 2. Invoice to paid through markAsPaid()
 myInvoice.markAsPaid()
-console.log("Status actual:", myInvoice.status)
+console.log("Current status:", myInvoice.status)
 
-// 3. Again markAsPaid() to have the already paid advice
+// 3. Again markAsPaid() to get the already paid notice
 console.log(myInvoice.markAsPaid())
 
-// 4. changeAmount() to change Invoice amount
-myInvoice.changeAmount(750)
-console.log("New Invoice amount:", myInvoice.amount) // 750
+// 4. changeAmount() to change Invoice amount to an even larger number
+myInvoice.changeAmount(99_999_999_999)
+console.log(Number.MAX_SAFE_INTEGER)
+console.log("New Invoice amount:", myInvoice.amount)
